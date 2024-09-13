e1bb4741ab0cc87a2a7e502660795e1b8b25472a
fisayo-dev <olufisayobadina@gmail.com>
2024-09-13 01:55:34 +0100

    Created all other pages

---
+++ frontend/src/pages/App/landing.jsx
@@ -0,0 +1,28 @@
+import { Button, Header, Footer } from "../../components";
+
+const Landing = ({message=''}) => {
+  return (
+    <div className="grid h-[100vh] ">
+      <Header />
+      <div className="app-container">
+        <div className="grid justify-items-center place-items-center h-full px-4 md:px-10">
+          <div className="mx-auto md:w-4/5 gap-4 grid text-center justify-items-center">
+            <h2 className="text-4xl md:text-5xl font-bold">
+              {/* Build a strong bond with all your buddies from anywhere. */}
+              {message}
+            </h2>
+            <p className="text-1xl">
+              Get in touch with your friends and families from any distance.
+            </p>
+            <div>
+              <Button styles="app-dark-text-color ">Get Started</Button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Landing;
