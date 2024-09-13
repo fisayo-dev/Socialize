6809bccdf2832453c899cd7ec528412e8bdf062c
fisayo-dev <olufisayobadina@gmail.com>
2024-09-13 17:11:44 +0100

    Made the signup page

--- frontend/src/pages/App/signup.jsx
+++ frontend/src/pages/App/signup.jsx
@@ -1,6 +1,10 @@
 const Signup = () => {
   return (
-    <div>Signup</div>
+    <div className="app-container backdrop-blur-md ">
+        <div className="py-10 items-center justify-center text-center flex">
+          <h1 className="text-1xl font-bold">Socialize</h1>
+        </div>
+      </div>
   )
 }
 
