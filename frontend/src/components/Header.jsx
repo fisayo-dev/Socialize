8706b3a7098bf8442ed59408e9b6d5c447a61dfe
fisayo-dev <olufisayobadina@gmail.com>
2024-09-13 17:07:09 +0100

    Modified the header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,12 +1,9 @@
-import Button from "./Button";
-
 const Header = () => {
   return (
     <div className="fixed top-0 w-full">
       <div className="app-container backdrop-blur-md ">
         <div className="py-10 items-center justify-center text-center flex">
           <h1 className="text-1xl font-bold">Socialize</h1>
-          {/* <Button styles="text-slate-900">Login</Button> */}
         </div>
       </div>
     </div>
