af7937ff5b3d3ed22904281c21d85d588e0e8fdf
fisayo-dev <olufisayobadina@gmail.com>
2024-09-13 17:31:22 +0100

    Gave the btn component a text color class

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Button = ({ children, styles }) => {
   return (
     <button
-      className={`bg-slate-200 font-extrabold rounded-lg px-4 py-2  hover:bg-slate-300 ${styles}`}
+      className={`bg-slate-200 font-extrabold rounded-lg px-4 py-2  hover:bg-slate-300 text-slate-900 ${styles}`}
     >
       {children}
     </button>
