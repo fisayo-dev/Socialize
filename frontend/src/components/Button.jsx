b3427f57e473bbbfe9bbe47c97c802131fb97041
fisayo-dev <olufisayobadina@gmail.com>
2024-09-13 22:42:49 +0100

    Finalised the procedural singup page

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 
-const Button = ({ children, styles }) => {
+const Button = ({ children, onClick = () => {}, styles }) => {
   return (
     <button
       className={`bg-slate-200 font-extrabold rounded-lg px-4 py-2  hover:bg-slate-300 text-slate-900 ${styles}`}
+      onClick={onClick}
     >
       {children}
     </button>
--- frontend/src/pages/App/signup.jsx
+++ frontend/src/pages/App/signup.jsx
@@ -17,13 +17,14 @@ const Signup = () => {
     Array(labels2.length).fill(false)
   );
 
-  // Create an array of refs for each input
+  // Create an array of refs for each input - 1st phase
   const inputRefs = Array.from({ length: labels1.length }, () => useRef(null));
   const [focusedInputIndexes, setFocusedInputIndexes] = useState(
     Array(labels1.length).fill(false)
   ); // Track focus for each input separately
 
-  const inputRefs2 = Array.from({ length: labels1.length }, () => useRef(null));
+  // Create an array of refs for each input - 2nd phase
+  const inputRefs2 = Array.from({ length: labels2.length }, () => useRef(null));
   const [focusedInputIndexes2, setFocusedInputIndexes2] = useState(
     Array(labels2.length).fill(false)
   ); // Track focus for each input separately
@@ -31,13 +32,17 @@ const Signup = () => {
   // Function to handle focus for a specific input
   const handleInputFocus = (index) => {
     const newFocusStates = [...focusedInputIndexes];
+    const newFocusStates2 = [...focusedInputIndexes2];
     newFocusStates[index] = true; // Set the focused input to true
+    newFocusStates2[index] = true; // Set the focused input to true
     setFocusedInputIndexes(newFocusStates);
+    setFocusedInputIndexes2(newFocusStates2);
   };
 
   // Function to handle clicks outside each input and blur it
   const handleClickOutside = (event) => {
     const newFocusStates = [...focusedInputIndexes];
+    const newFocusStates2 = [...focusedInputIndexes2];
     inputRefs.forEach((inputRef, index) => {
       // Prevent blur if input is filled
       if (
@@ -48,7 +53,18 @@ const Signup = () => {
         newFocusStates[index] = false; // Blur only if input is empty
       }
     });
+    inputRefs2.forEach((inputRef, index) => {
+      // Prevent blur if input is filled
+      if (
+        inputRef.current &&
+        !inputRef.current.contains(event.target) &&
+        inputRef.current.value.trim() === ""
+      ) {
+        newFocusStates2[index] = false; // Blur only if input is empty
+      }
+    });
     setFocusedInputIndexes(newFocusStates);
+    setFocusedInputIndexes2(newFocusStates2);
   };
 
   // Add an event listener to detect clicks outside each input
@@ -63,12 +79,15 @@ const Signup = () => {
   const handleNextClick = (e) => {
     e.preventDefault();
     const currentErrors = [...errorInputIndex1];
-    let hasError = false;
+    const currentErrors2 = [...errorInputIndex2];
+
+    let hasError1 = false;
+    let hasError2 = false;
 
     inputRefs.forEach((inputRef, index) => {
       if (inputRef.current && inputRef.current.value.trim() === "") {
         currentErrors[index] = true; // Mark error for empty input
-        hasError = true;
+        hasError1 = true;
       } else {
         currentErrors[index] = false; // No error if filled
         setCurrentPageIndex(1);
@@ -82,13 +101,40 @@ const Signup = () => {
       }
     });
 
+    inputRefs2.forEach((inputRef, index) => {
+      if (inputRef.current && inputRef.current.value.trim() === "") {
+        currentErrors2[index] = true; // Mark error for empty input
+        hasError2 = true;
+      } else {
+        currentErrors2[index] = false; // No error if filled
+        setCurrentPageIndex(1);
+      }
+
+      if (currentPageIndex == 1) {
+        setCurrentPageIndex(2);
+      }
+      if (currentPageIndex == 2) {
+        setCurrentPageIndex(3);
+      }
+    });
+
     setErrorInputIndex1(currentErrors);
+    setErrorInputIndex2(currentErrors2);
 
     // Proceed if there are no errors
-    if (!hasError) {
+    if (!hasError1) {
       console.log("All inputs are filled. Proceed to the next step.");
       // You can add your next step logic here
     }
+    if (!hasError2) {
+      console.log("All Recoginize filleds. Proceed to the next step.");
+      // You can add your next step logic here
+    }
+  };
+
+  const handlePreviousClick = (e) => {
+    e.preventDefault();
+    setCurrentPageIndex((currentIndex) => currentIndex - 1);
   };
 
   const getInputType = (label) => {
@@ -122,119 +168,131 @@ const Signup = () => {
               id="signup_form"
               className="w-full grid gap-6 md:w-2/4 mx-auto"
             >
-              {currentPageIndex == 0 &&
-                labels1.map((label, index) => (
-                  <div key={index} className="grid gap-5">
-                    <div
-                      key={index}
-                      className={`relative flex gap-5 ${
+              {labels1.map((label, index) => (
+                <div
+                  key={index}
+                  className={`${
+                    currentPageIndex == 0 ? "grid" : "hidden"
+                  } gap-5`}
+                >
+                  <div
+                    key={index}
+                    className={`relative flex gap-5 ${
+                      focusedInputIndexes[index]
+                        ? "border-slate-200"
+                        : "border-slate-500"
+                    } border-b-2 items-center text-[1rem] md:text-[1.12rem]`}
+                  >
+                    <h2
+                      className={`${
                         focusedInputIndexes[index]
-                          ? "border-slate-200"
-                          : "border-slate-500"
-                      } border-b-2 items-center text-[1rem] md:text-[1.12rem]`}
+                          ? "focus-up-text"
+                          : "focus-inp-text"
+                      }`}
                     >
-                      <h2
-                        className={`${
-                          focusedInputIndexes[index]
-                            ? "focus-up-text"
-                            : "focus-inp-text"
-                        }`}
-                      >
-                        {label}
-                      </h2>
-                      <input
-                        type={getInputType(label)}
-                        className={`w-full py-4 `}
-                        ref={inputRefs[index]}
-                        onFocus={() => handleInputFocus(index)}
-                        onChange={(e) => {
-                          const current = [...errorInputIndex1];
-                          if (e.target.value.trim() === "") {
-                            current[index] = true;
-                            setErrorInputIndex1(current);
-                          } else {
-                            current[index] = false;
-                            setErrorInputIndex1(current);
-                          }
-                        }}
-                      />
-                    </div>
-                    {errorInputIndex1[index] && (
-                      <p className="text-red-300">This field cannot be empty</p>
-                    )}
+                      {label}
+                    </h2>
+                    <input
+                      type={getInputType(label)}
+                      className={`w-full py-4 `}
+                      ref={inputRefs[index]}
+                      onFocus={() => handleInputFocus(index)}
+                      onChange={(e) => {
+                        const current = [...errorInputIndex1];
+                        if (e.target.value.trim() === "") {
+                          current[index] = true;
+                          setErrorInputIndex1(current);
+                        } else {
+                          current[index] = false;
+                          setErrorInputIndex1(current);
+                        }
+                      }}
+                    />
                   </div>
-                ))}
-
-              {currentPageIndex == 1 && (
-                <div
-                  className="relative flex gap-5 
-                  items-center text-[1rem] md:text-[1.12rem]"
-                >
-                  <select className="select font-bold w-full border-2 rounded-lg border-slate-200 p-3 ">
-                    <option value="">Male</option>
-                    <option value="">Female</option>
-                    <option value="">Others</option>
-                  </select>
+                  {errorInputIndex1[index] && (
+                    <p className="text-red-300">This field cannot be empty</p>
+                  )}
                 </div>
-              )}
-              {currentPageIndex == 2 && (
-                <div
-                  className="relative flex gap-5 
-                items-center text-[1rem] md:text-[1.12rem]"
-                >
-                  <select className="select font-bold w-full border-2 rounded-lg border-slate-200 p-3 ">
-                    <option value="">Nigeria</option>
-                    <option value="">Ukraine</option>
-                    <option value="">Russia</option>
-                    <option value="">Ghana</option>
-                    <option value="">Liberia</option>
-                  </select>
-                </div>
-              )}
+              ))}
 
-              {currentPageIndex == 3 &&
-                labels2.map((label, index) => (
-                  <div key={index} className="grid gap-5">
-                    <div
-                      key={index}
-                      className={`relative flex gap-5 ${
+              <div
+                className={`relative ${
+                  currentPageIndex == 1 ? "flex" : "hidden"
+                } gap-5 
+                  items-center text-[1rem] md:text-[1.12rem]`}
+              >
+                <select className="select font-bold w-full border-2 rounded-lg border-slate-200 p-3 ">
+                  <option value="">Male</option>
+                  <option value="">Female</option>
+                  <option value="">Others</option>
+                </select>
+              </div>
+
+              <div
+                className={`relative ${
+                  currentPageIndex == 2 ? "flex" : "hidden"
+                } gap-5 
+              items-center text-[1rem] md:text-[1.12rem]`}
+              >
+                <select className="select font-bold w-full border-2 rounded-lg border-slate-200 p-3 ">
+                  <option value="">Nigeria</option>
+                  <option value="">Ukraine</option>
+                  <option value="">Russia</option>
+                  <option value="">Ghana</option>
+                  <option value="">Liberia</option>
+                </select>
+              </div>
+
+              {labels2.map((label, index) => (
+                <div
+                  key={index}
+                  className={`${
+                    currentPageIndex == 3 ? "grid" : "hidden"
+                  } grid gap-5`}
+                >
+                  <div
+                    key={index}
+                    className={`relative flex gap-5 ${
+                      focusedInputIndexes2[index]
+                        ? "border-slate-200"
+                        : "border-slate-500"
+                    } border-b-2 items-center text-[1rem] md:text-[1.12rem]`}
+                  >
+                    <h2
+                      className={`${
                         focusedInputIndexes2[index]
-                          ? "border-slate-200"
-                          : "border-slate-500"
-                      } border-b-2 items-center text-[1rem] md:text-[1.12rem]`}
+                          ? "focus-up-text"
+                          : "focus-inp-text"
+                      }`}
                     >
-                      <h2
-                        className={`${
-                          focusedInputIndexes2[index]
-                            ? "focus-up-text"
-                            : "focus-inp-text"
-                        }`}
-                      >
-                        {label}
-                      </h2>
-                      <input
-                        type={getInputType(label)}
-                        className={`w-full py-4 `}
-                        ref={inputRefs2[index]}
-                        onFocus={() => handleInputFocus(index)}
-                        onChange={(e) => {
-                          const current = [...errorInputIndex2];
-                          if (e.target.value.trim() === "") {
-                            current[index] = true;
-                            setErrorInputIndex2(current);
-                          } else {
-                            current[index] = false;
-                            setErrorInputIndex2(current);
-                          }
-                        }}
-                      />
-                    </div>
-                    {errorInputIndex2[index] && (
-                      <p className="text-red-300">This field cannot be empty</p>
-                    )}
+                      {label}
+                    </h2>
+                    <input
+                      type={getInputType(label)}
+                      className={`w-full py-4 `}
+                      ref={inputRefs2[index]}
+                      onFocus={() => handleInputFocus(index)}
+                      onChange={(e) => {
+                        const current = [...errorInputIndex2];
+                        if (e.target.value.trim() === "") {
+                          current[index] = true;
+                          setErrorInputIndex2(current);
+                        } else {
+                          current[index] = false;
+                          setErrorInputIndex2(current);
+                        }
+                      }}
+                    />
                   </div>
-                ))}
-              <div className="grid gap-5 py-5 items-center">
+                  {errorInputIndex2[index] && (
+                    <p className="text-red-300">This field cannot be empty</p>
+                  )}
+                </div>
+              ))}
+              <div className="flex gap-5 py-5 items-center w-full">
+                {currentPageIndex > 0 && (
+                  <Button onClick={handlePreviousClick}> Previous</Button>
+                )}
                 <Button onClick={handleNextClick}>Next</Button>
               </div>
             </form>
