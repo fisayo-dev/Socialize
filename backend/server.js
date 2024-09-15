44630d2181cd049689b0a647a326a08a064ba4e4
fisayo-dev <olufisayobadina@gmail.com>
2024-09-16 00:48:40 +0100

    linked api user route to users router

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,16 @@
 import express from 'express';
+import usersRouter from './routes/usersRouter.js'
 
 const app = express();
 
 const port = process.env.PORT || 7000;
 
-
 // Sample API route
-app.get('/api', (req, res) => {
+app.get('/api/', (req, res) => {
   res.json({ message: "Where Connections Spark and Conversations Thrive!" });
 });
+
+app.use('/api/users', usersRouter)
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
