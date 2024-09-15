3f08c1c4bc590a136a0d4ed4b3ba1912592b3f54
fisayo-dev <olufisayobadina@gmail.com>
2024-09-16 00:52:07 +0100

    Added create user route function

--- backend/routes/usersRouter.js
+++ backend/routes/usersRouter.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import { getUsers, getUser, updateUser, deleteUser } from '../controllers/userControllers.js'
+import { getUsers, getUser, updateUser, deleteUser, createUser } from '../controllers/userControllers.js'
 
 const router = express.Router()
 
 router.get('/', getUsers)
 router.get('/:id', getUser)
+router.post('/', createUser)
 router.put('/', updateUser)
 router.delete('/', deleteUser)
 
