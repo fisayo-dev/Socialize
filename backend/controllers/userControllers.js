f871598d920ec42e4171b7daca205bc4f0341890
fisayo-dev <olufisayobadina@gmail.com>
2024-09-16 00:50:11 +0100

    Created controllers for all user crud routers

---
+++ backend/controllers/userControllers.js
@@ -0,0 +1,7 @@
+const getUsers = (req, res) => {}
+const getUser = (req, res) => {}
+const createUser = (req, res) => {}
+const updateUser = (req, res) => {}
+const deleteUser = (req, res) => {}
+
+export {getUser,getUsers,createUser,updateUser,deleteUser}
