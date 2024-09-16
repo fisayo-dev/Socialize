53b95613760843ed685fc5d815e746e37d7941f3
fisayo-dev <olufisayobadina@gmail.com>
2024-09-16 20:18:30 +0100

    created this component

--- frontend/src/components/ChatProfile.jsx
+++ frontend/src/components/ChatProfile.jsx
@@ -2,7 +2,7 @@ import { useParams } from "react-router-dom";
 
 const ChatProfile = () => {
   const { id } = useParams();
-  return <div>ChatProfile - {id}</div>;
+  return <div className="grid bg-black bg-opacity-20 h-[100vh]">ChatProfile - {id}</div>;
 };
 
 export default ChatProfile;
