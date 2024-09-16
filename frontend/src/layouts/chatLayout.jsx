b467c1da1a2f8da71b437fb614a09065469ee48d
fisayo-dev <olufisayobadina@gmail.com>
2024-09-16 19:01:25 +0100

    imported chat layout components

--- frontend/src/layouts/chatLayout.jsx
+++ frontend/src/layouts/chatLayout.jsx
@@ -1,8 +1,11 @@
 import { Outlet } from "react-router-dom";
+import { ChatProfile, ChatSidebar } from "../components";
 const ChatLayout = () => {
   return (
     <div>
+      <ChatSidebar />
       <Outlet />
+      <ChatProfile/>
     </div>
   );
 };
