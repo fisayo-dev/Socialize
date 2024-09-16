e8f3f31886662c4b51b0fb41ce384d4cade1db30
fisayo-dev <olufisayobadina@gmail.com>
2024-09-16 20:19:24 +0100

    created chat sidebar content component

--- frontend/src/components/ChatSidebar.jsx
+++ frontend/src/components/ChatSidebar.jsx
@@ -1,7 +1,73 @@
+import { FaUser, FaPlus, FaMagnifyingGlass } from "react-icons/fa6";
+
+import { AiOutlineUser } from "react-icons/ai";
+import { useState } from "react";
+
 const ChatSidebar = () => {
+  const [friends, setFriends] = useState([]);
+  const [requests, setRequests] = useState([]);
+  const [usersRender, setUsersRender] = useState(0);
+
   return (
-    <div>ChatSidebar</div>
-  )
-}
+    <div className="bg-black bg-opacity-20 overflow-x-hidden h-[100vh] ">
+      <div className="px-2 grid gap-2">
+        <div className="grid ">
+          <div className="flex py-3 items-center">
+            <h2 className="w-full font-bold text-[1.2rem]">Socialize</h2>
+            <div className="flex items-center gap-2">
+              <FaPlus />
+              <FaUser />
+            </div>
+          </div>
+          <div className="bg-slate-800 rounded-lg p-3 text-[0.92rem]">
+            <div className="flex items-center gap-1">
+              <FaMagnifyingGlass />
+              <input
+                type="text"
+                className="w-full"
+                placeholder="Search for friends"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-start gap-5 items-center py-1">
+          <h2
+            className={`cursor-pointer ${
+              usersRender == 0 && "border-b-2 border-slate-200 font-bold"
+            } hover:font-bold`}
+            onClick={() => setUsersRender(0)}
+          >
+            Friends
+          </h2>
+          <h2
+            className={`cursor-pointer ${
+              usersRender == 1 && "border-b-2 border-slate-200 font-bold"
+            } hover:font-bold`}
+            onClick={() => setUsersRender(1)}
+          >
+            Requests
+          </h2>
+        </div>
+      </div>
+
+      <div className="grid py-2">
+        <div className="bg-slate-800 p-3">
+          <div className="flex gap-5 items-center">
+            <div>
+              <AiOutlineUser size={30} />
+            </div>
+            <div className="grid w-full">
+              <h2 className="text-md font-bold">Fisayo</h2>
+              <p className="text-sm">I will see you later.</p>
+            </div>
+            <div className="text-sm flex bg-slate-400 px-2 justify-center rounded-full">
+              1
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default ChatSidebar;
