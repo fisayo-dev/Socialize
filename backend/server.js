253228d7d8ef6371b906a57b9e057eb029cee875
fisayo-dev <olufisayobadina@gmail.com>
2024-09-16 18:16:30 +0100

    Added JSON and body data middleware

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
-import express from 'express';
+import express, { json, urlencoded } from 'express';
 import usersRouter from './routes/usersRouter.js'
 
 const app = express();
 
 const port = process.env.PORT || 7000;
+
+// Json and body data middleware
+app.use(express.json())
+app.use(urlencoded({extended: false}))
 
 // Sample API route
 app.get('/api/', (req, res) => {
