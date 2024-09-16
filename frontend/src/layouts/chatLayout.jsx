3c1c716967fa138bed88df2ef758196c276e14f5
fisayo-dev <olufisayobadina@gmail.com>
2024-09-16 20:17:54 +0100

    used chat layout

--- frontend/src/layouts/chatLayout.jsx
+++ frontend/src/layouts/chatLayout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 import { ChatProfile, ChatSidebar } from "../components";
 const ChatLayout = () => {
   return (
-    <div>
+    <div className="chats-app-flex">
       <ChatSidebar />
       <Outlet />
       <ChatProfile />
