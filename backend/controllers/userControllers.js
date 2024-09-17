192169479e7b00ac31e7671c3e64c26ff3eeb848
fisayo-dev <olufisayobadina@gmail.com>
2024-09-17 17:17:11 +0100

    implemented get requests

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -2,81 +2,111 @@ const fakeUsers = [
     {
         id: 1,
         name: 'Fisayo',
-        email: 'test@test.com',
+        email: 'fisayo@fisayo.com',
         friends: [
             101
+        ],
+        requests: [
+            100
         ]
     },
     {
         id: 2,
         name: 'Daniel',
-        email: 'test@test.com',
+        email: 'daniel@daniel.com',
         friends: [
             101
+        ],
+        requests: [
+            100
         ]
     },
     {
         id: 3,
         name: 'Sofia',
-        email: 'test@test.com',
+        email: 'sofia@sofia.com',
         friends: [
             101
+        ],
+        requests: [
+            100
         ]
     },
     {
         id: 4,
-        name: 'Daniel',
-        email: 'test@test.com',
+        name: 'Gbenga',
+        email: 'gbenga@gbenga.com',
         friends: [
             101
+        ],
+        requests: [
+            100
         ]
     },
     {
         id: 5,
         name: 'Elizabeth',
-        email: 'test@test.com',
+        email: 'lizzy@gmail.com',
         friends: [
             100,
+        ],
+          requests: [
+            101
         ]
     },
     {
         id: 6,
         name: 'Demilade',
-        email: 'test@test.com',
+        email: 'ladezy@gmail.com',
         friends: [
             100,
+        ],
+          requests: [
+            101
         ]
     },
     {
         id: 7,
         name: 'Ayokunle',
-        email: 'test@test.com',
+        email: 'kunzi@yahoo.com',
         friends: [
             100,
+        ],
+          requests: [
+            101
         ]
     },
     {
         id: 8,
         name: 'Esther',
-        email: 'test@test.com',
+        email: 'kings@gmail.com',
         friends: [
             100,
+        ],
+          requests: [
+            101
         ]
     },
     {
         id: 9,
         name: 'Bisilola',
-        email: 'test@test.com',
+        email: 'bode@gmail.com',
         friends: [
             100,
+        ],
+          requests: [
+            101
         ]
     },
     {
         id: 10,
         name: 'Folusho',
-        email: 'test@test.com',
+        email: 'shola@yahoo.com',
         friends: [
             100,
+        ],
+          requests: [
+            101
         ]
     },
 ]
@@ -85,15 +115,31 @@ const getUsers = (req, res) => {
 }
 const getFriends = (req, res) => {
     const personId = parseInt(req.body.personId );
-    console.log(personId)
-    // Get Friends of requesting user
+    console.log(personId);
     const fetchedFriends = fakeUsers.filter((user) => user.friends.includes(personId))
     console.log(fetchedFriends)
     res.status(200).json(fetchedFriends)
+}
+const getFriendRequests = (req, res) => {
+    const friendID = parseInt(req.body.personId);
+    const fetchedRequestingFriends = fakeUsers.filter((user) => user.requests.includes(friendID));
+    console.log(fetchedRequestingFriends)
+    res.status(200).json(fetchedRequestingFriends);
+}
+const acceptRequest = (req, res) => {
+    const personId = req.body.personId
+    const requestingUserId = req.body.requestingId;
+
+    const particluarRequestingUser = fakeUsers.filter((user) => user.id == requestingUserId)
+    console.log(particluarRequestingUser)
+    // particluarRequestingUser.requests.splice(0, personId)
+    // particluarRequestingUser.friends.push(personId)
+
+    res.status(200).json(particluarRequestingUser)
 }
 const getUser = (req, res) => {}
 const createUser = (req, res) => {}
 const updateUser = (req, res) => {}
 const deleteUser = (req, res) => {}
 
-export {getUser,getUsers,createUser,updateUser,deleteUser, getFriends}
+export {getUser,getUsers,createUser,updateUser,deleteUser, getFriends, getFriendRequests, acceptRequest}
