6d625e4596dbf4fdeec5f3b06087a5d983e175f3
fisayo-dev <olufisayobadina@gmail.com>
2024-09-17 17:20:58 +0100

    implemented fetch friend request functionality and other UI updates

--- frontend/src/components/ChatSidebar.jsx
+++ frontend/src/components/ChatSidebar.jsx
@@ -1,16 +1,23 @@
 import { FaUser, FaPlus, FaMagnifyingGlass } from "react-icons/fa6";
-
 import { AiOutlineUser } from "react-icons/ai";
 import { useState, useEffect } from "react";
+import { Link, NavLink } from "react-router-dom";
+import { acceptRequest } from "../../../backend/controllers/userControllers";
 
 const ChatSidebar = () => {
   const [friends, setFriends] = useState([]);
   const [filteredFriends, setFilteredFriends] = useState([]);
   const [requests, setRequests] = useState([]);
-  const [usersRender, setUsersRender] = useState(0);
+  const [filteredRequests, setFilteredRequests] = useState([]);
+  const [usersRender, setUsersRender] = useState(1);
   const [searchValue, setSearchValue] = useState("");
+  const [searchRequestValue, setSearchRequestValue] = useState("");
 
   const personId = 100;
+
+  const linkStatus = ({ isActive }) => {
+    return isActive ? "bg-gray-800" : "hover:bg-gray-700";
+  };
 
   const searchFriends = () => {
     const listOfFriends = friends.filter((friend) =>
@@ -18,12 +25,28 @@ const ChatSidebar = () => {
     );
     setFilteredFriends(listOfFriends);
   };
+  const searchRequests = () => {
+    const listOfRequests = requests.filter((request) =>
+      request.name
+        .toLocaleLowerCase()
+        .includes(searchRequestValue.toLocaleLowerCase())
+    );
+    setFilteredRequests(listOfRequests);
+  };
 
-  useEffect(() => {
-    searchFriends();
-  }, [searchValue]);
-
-  const fetchUsers = async () => {
+  const fetchFriendRequests = async () => {
+    const res = await fetch("/api/users/requests", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ personId }),
+    });
+    const friendRequests = await res.json();
+    setRequests(friendRequests);
+    setFilteredRequests(friendRequests);
+  };
+  const fetchFriends = async () => {
     const res = await fetch("/api/users/friends", {
       method: "POST",
       headers: {
@@ -34,10 +57,30 @@ const ChatSidebar = () => {
     const friends = await res.json();
     setFriends(friends);
     setFilteredFriends(friends);
-    console.log(friends);
+  };
+
+  const acceptRequest = async (requestingId) => {
+    const res = await fetch("/api/users/friends", {
+      method: "PUT",
+      header: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ requestingId, personId }),
+    });
+    const newFriendsList = await res.json();
+    setFriends(newFriendsList);
+    setFilteredFriends(newFriendsList);
   };
   useEffect(() => {
-    fetchUsers();
+    searchFriends();
+  }, [searchValue]);
+
+  useEffect(() => {
+    searchRequests();
+  }, [searchRequestValue]);
+  useEffect(() => {
+    fetchFriends();
+    fetchFriendRequests();
   }, []);
 
   return (
@@ -51,18 +94,34 @@ const ChatSidebar = () => {
               <FaUser />
             </div>
           </div>
-          <div className="bg-slate-800 rounded-lg p-3 text-[0.92rem]">
-            <div className="flex items-center gap-1">
-              <FaMagnifyingGlass />
-              <input
-                type="text"
-                className="w-full"
-                placeholder="Search for friends"
-                value={searchValue}
-                onChange={(e) => setSearchValue(e.target.value)}
-              />
+          {usersRender == 0 && (
+            <div className="bg-slate-800 rounded-lg p-3 text-[0.92rem]">
+              <div className="flex items-center gap-1">
+                <FaMagnifyingGlass />
+                <input
+                  type="text"
+                  className="w-full"
+                  placeholder="Search friends by name"
+                  value={searchValue}
+                  onChange={(e) => setSearchValue(e.target.value)}
+                />
+              </div>
             </div>
-          </div>
+          )}
+          {usersRender == 1 && (
+            <div className="bg-slate-800 rounded-lg p-3 text-[0.92rem]">
+              <div className="flex items-center gap-1">
+                <FaMagnifyingGlass />
+                <input
+                  type="text"
+                  className="w-full"
+                  placeholder="Search your requests"
+                  value={searchRequestValue}
+                  onChange={(e) => setSearchRequestValue(e.target.value)}
+                />
+              </div>
+            </div>
+          )}
         </div>
         <div className="flex justify-start gap-5 items-center py-1">
           <h2
@@ -84,23 +143,84 @@ const ChatSidebar = () => {
         </div>
       </div>
 
-      <div className="grid gap-1 py-2">
+      <div className="grid gap py-2">
+        {usersRender === 0 && friends.length == 0 ? (
+          <div className="text-center py-5 font-bold">
+            <div>You have no friends.</div>
+            <Link></Link>
+          </div>
+        ) : (
+          usersRender === 0 &&
+          filteredFriends.length == 0 && (
+            <div className="text-center py-5 font-bold">
+              Couldn't find friend
+            </div>
+          )
+        )}
+        {usersRender === 1 && requests.length == 0 ? (
+          <div className="text-center py-5 font-bold">
+            <h1>No friends request available.</h1>
+            <Link></Link>
+          </div>
+        ) : (
+          usersRender === 1 &&
+          filteredRequests.length == 0 && (
+            <div className="text-center py-5 font-bold">
+              No friend requests.
+            </div>
+          )
+        )}
+
         {usersRender === 0 &&
+          filteredFriends.length !== 0 &&
           filteredFriends.map((friend) => (
-            <div key={friend.id} className="bg-slate-800 p-3">
+            <NavLink to={`./chats/${friend.id}`} className={linkStatus}>
+              <div
+                key={friend.id}
+                className="cursor-pointer p-3"
+              >
+                <div className="flex gap-5 items-center">
+                  <div>
+                    <AiOutlineUser size={30} />
+                  </div>
+                  <div className="grid w-full">
+                    <h2 className="text-md font-bold">{friend.name}</h2>
+                    <p className="text-sm">{friend.email}</p>
+                  </div>
+                  <div className="text-sm flex bg-slate-400 text-slate-900 px-2 justify-center rounded-full">
+                    5
+                  </div>
+                </div>
+              </div>
+            </NavLink>
+          ))}
+        {usersRender === 1 &&
+          filteredRequests.length !== 0 &&
+          filteredRequests.map((request) => (
+            <NavLink className={linkStatus} to={`/chats/${request.id}`}>
+
+            <div
+              key={request.id}
+              className="cursor-pointer hover:bg-slate-800 p-3"
+            >
               <div className="flex gap-5 items-center">
                 <div>
                   <AiOutlineUser size={30} />
                 </div>
                 <div className="grid w-full">
-                  <h2 className="text-md font-bold">{friend.name}</h2>
-                  <p className="text-sm">I will see you later.</p>
+                  <h2 className="text-md font-bold">{request.name}</h2>
+                  <p className="text-sm">{request.email}</p>
                 </div>
-                <div className="text-sm flex bg-slate-400 px-2 justify-center rounded-full">
-                  1
+                <div
+                  onClick={acceptRequest}
+                  className="text-sm flex bg-slate-400 hover:bg-slate-200 text-slate-900 px-2 py-1 justify-center rounded-full cursor-pointer"
+                >
+                  Accept
                 </div>
               </div>
-            </div>
+              </div>
+            </NavLink>
+              
           ))}
       </div>
     </div>
