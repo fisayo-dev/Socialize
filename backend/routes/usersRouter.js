44a6ca871c93cbe87087b3b66fc890185a922524
fisayo-dev <olufisayobadina@gmail.com>
2024-09-17 17:18:05 +0100

    Added route to get request and accept request

--- backend/routes/usersRouter.js
+++ backend/routes/usersRouter.js
@@ -1,13 +1,22 @@
 import express from 'express'
-import { getUsers, getUser, updateUser, deleteUser, createUser, getFriends } from '../controllers/userControllers.js'
+import { getUsers, getUser, updateUser, deleteUser, createUser, getFriends, getFriendRequests, acceptRequest } from '../controllers/userControllers.js'
 
 const router = express.Router()
 
+// get Routes
 router.get('/', getUsers)
-router.post('/friends', getFriends)
 router.get('/:id', getUser)
+
+// Create Routes
+router.post('/friends', getFriends)
+router.post('/requests', getFriendRequests)
 router.post('/', createUser)
+
+// Update Routes
 router.put('/', updateUser)
+router.put('/friends', acceptRequest)
+
+// Delete Routes
 router.delete('/', deleteUser)
 
 
