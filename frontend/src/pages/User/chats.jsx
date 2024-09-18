3ad1dc881ae97d5cbfb1b067b94cfe264cc95c8b
fisayo-dev <olufisayobadina@gmail.com>
2024-09-18 13:43:02 +0100

    made the chat landing page for user.

--- frontend/src/pages/User/chats.jsx
+++ frontend/src/pages/User/chats.jsx
@@ -1,7 +1,10 @@
 const Chats = () => {
   return (
-    <div className="grid  h-[100vh]">
-      <h1>Hi</h1>
+    <div className="grid place-items-center h-[100vh]">
+      <div className="text-center">
+        <h2 className="text-4xl md:text-6xl lg:text-7xl font-bold">Socialize</h2>
+        <p className="text-md text-slate-400">Choose a friend to start chatting.</p>
+      </div>
     </div>
   );
 };
