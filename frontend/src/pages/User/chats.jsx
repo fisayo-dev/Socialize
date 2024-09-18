141f3ea6474a5aebcb2e426908354621093cb544
fisayo-dev <olufisayobadina@gmail.com>
2024-09-18 13:49:11 +0100

    styled it up a bit

--- frontend/src/pages/User/chats.jsx
+++ frontend/src/pages/User/chats.jsx
@@ -1,9 +1,21 @@
+import { Link } from "react-router-dom";
+import Button from "../../components/Button";
+
 const Chats = () => {
   return (
     <div className="grid place-items-center h-[100vh]">
-      <div className="text-center">
-        <h2 className="text-4xl md:text-6xl lg:text-7xl font-bold">Socialize</h2>
-        <p className="text-md text-slate-400">Choose a friend to start chatting.</p>
+      <div className="grid gap-1 text-center">
+        <div>
+          <h2 className="text-4xl md:text-5xl lg:text-6xl font-bold">
+            Socialize
+          </h2>
+        </div>
+          <p className="text-md text-slate-400">
+            Choose a friend to start chatting.
+          </p>
+        <Link to="/" className="mt-1">
+          <Button>Create Friend</Button>
+        </Link>
       </div>
     </div>
   );
