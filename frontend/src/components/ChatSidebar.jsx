ed15283e33f6dc11b19fc5960e6ea2d8e2b61558
fisayo-dev <olufisayobadina@gmail.com>
2024-09-18 13:32:54 +0100

    added the active chat user color indicator functionality

--- frontend/src/components/ChatSidebar.jsx
+++ frontend/src/components/ChatSidebar.jsx
@@ -1,23 +1,23 @@
 import { FaUser, FaPlus, FaMagnifyingGlass } from "react-icons/fa6";
 import { AiOutlineUser } from "react-icons/ai";
 import { useState, useEffect } from "react";
-import { Link, NavLink } from "react-router-dom";
-import { acceptRequest } from "../../../backend/controllers/userControllers";
+import { Link, NavLink, useParams } from "react-router-dom";
 
 const ChatSidebar = () => {
+
+  const {id} = useParams()
   const [friends, setFriends] = useState([]);
   const [filteredFriends, setFilteredFriends] = useState([]);
   const [requests, setRequests] = useState([]);
   const [filteredRequests, setFilteredRequests] = useState([]);
-  const [usersRender, setUsersRender] = useState(1);
+  const [usersRender, setUsersRender] = useState(0);
   const [searchValue, setSearchValue] = useState("");
   const [searchRequestValue, setSearchRequestValue] = useState("");
 
   const personId = 100;
 
-  const linkStatus = ({ isActive }) => {
-    return isActive ? "bg-gray-700" : "hover:bg-gray-800";
-  };
+  const linkStatus = ({ isActive }) =>
+    isActive ? "bg-slate-700" : "hover:bg-slate-800";
 
   const searchFriends = () => {
     const listOfFriends = friends.filter((friend) =>
@@ -62,14 +62,21 @@ const ChatSidebar = () => {
   const acceptRequest = async (requestingId) => {
     const res = await fetch("/api/users/friends", {
       method: "PUT",
-      header: {
+      headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({ requestingId, personId }),
     });
     const newFriendsList = await res.json();
+    const newFriendRequestsList = requests.filter(
+      (requestUser) => requestUser.id !== requestingId
+    );
+
     setFriends(newFriendsList);
     setFilteredFriends(newFriendsList);
+
+    setRequests(newFriendRequestsList);
+    setFilteredRequests(newFriendRequestsList);
   };
   useEffect(() => {
     searchFriends();
@@ -84,9 +91,9 @@ const ChatSidebar = () => {
   }, []);
 
   return (
-    <div className="bg-black bg-opacity-20 overflow-x-hidden h-[100vh] ">
-      <div className="px-2 grid gap-2">
-        <div className="grid ">
+    <div className="app-dark-bg-color relative side-bar-grid overflow-x-hidden h-[100vh] ">
+      <div className=" px-5 grid gap-2">
+        <div className="grid w-full">
           <div className="flex py-3 items-center">
             <h2 className="w-full font-bold text-[1.2rem]">Socialize</h2>
             <div className="flex items-center gap-2">
@@ -143,7 +150,7 @@ const ChatSidebar = () => {
         </div>
       </div>
 
-      <div className="grid gap py-2">
+      <div className=" overflow-y-scroll py-3">
         {usersRender === 0 && friends.length == 0 ? (
           <div className="text-center py-5 font-bold">
             <div>You have no friends.</div>
@@ -173,9 +180,11 @@ const ChatSidebar = () => {
 
         {usersRender === 0 &&
           filteredFriends.length !== 0 &&
-          filteredFriends.map((friend) => (
-            <NavLink to={`./chats/${friend.id}`} className={linkStatus}>
-              <div key={friend.id} className="cursor-pointer p-3">
+          filteredFriends.map(friend => 
+            <Link
+              key={friend.id} to={`chats/${friend.id}`}
+            >
+              <div className={`cursor-pointer  px-5 py-3 ${id == friend.id ? 'bg-slate-800' : 'hover:bg-slate-900'} `}>
                 <div className="flex gap-5 items-center">
                   <div>
                     <AiOutlineUser size={30} />
@@ -189,12 +198,15 @@ const ChatSidebar = () => {
                   </div>
                 </div>
               </div>
-            </NavLink>
-          ))}
+            </Link>
+          )}
         {usersRender === 1 &&
           filteredRequests.length !== 0 &&
           filteredRequests.map((request) => (
-            <div key={request.id} className="cursor-pointer hover:bg-gray-800 p-3">
+            <div
+              key={request.id}
+              className="cursor-pointer hover:bg-gray-800 p-3"
+            >
               <div className="flex gap-5 items-center">
                 <div>
                   <AiOutlineUser size={30} />
@@ -204,7 +216,7 @@ const ChatSidebar = () => {
                   <p className="text-sm">{request.email}</p>
                 </div>
                 <div
-                  onClick={acceptRequest}
+                  onClick={acceptRequest.bind(this, request.id)}
                   className="text-sm flex bg-slate-400 hover:bg-slate-200 text-slate-900 px-2 py-1 justify-center rounded-full cursor-pointer"
                 >
                   Accept
