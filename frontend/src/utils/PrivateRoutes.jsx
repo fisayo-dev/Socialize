ac88c1fadab2d0d97de582c3055818a7ca78b26b
fisayo-dev <olufisayobadina@gmail.com>
2024-09-21 20:32:07 +0100

    created private route component

--- frontend/src/utils/PrivateRoutes.jsx
+++ frontend/src/utils/PrivateRoutes.jsx
@@ -2,7 +2,7 @@ import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 
 const PrivateRoutes = () => {
-    const {user} = useAuth();
+  const { user } = useAuth();
   return user ? <Outlet /> : <Navigate to="/login" />;
 };
 
