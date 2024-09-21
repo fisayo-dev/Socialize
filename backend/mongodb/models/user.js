9df7726fcd40baf4c670b179df376400c705dfd5
fisayo-dev <olufisayobadina@gmail.com>
2024-09-21 20:31:09 +0100

    created user model

--- backend/mongodb/models/user.js
+++ backend/mongodb/models/user.js
@@ -7,7 +7,7 @@ const UserSchema = new mongoose.Schema({
     profile_img: {type: String, required: true},
     gender: {type: String, required: true},
     country: {type: String, required: true},
-    dob: {type: Date, required: true},
+    dob: {type: String, required: true},
     email: {type: String, required: true},
     password: {type: String, required: true},
     friends: [{type: String, required: true}],
