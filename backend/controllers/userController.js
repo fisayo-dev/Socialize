e5061b547b6bcd9b6a38b243e55e693205568789
fisayo-dev <olufisayobadina@gmail.com>
2024-09-21 20:30:03 +0100

    created the user controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,3 +1,12 @@
+import User from '../mongodb/models/user.js'
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const SECRET_KEY = process.env.SECRET_KEY;
+
 const fakeUsers = [
     {
         id: 1,
@@ -143,8 +152,40 @@ const acceptRequest = (req, res) => {
     users.push(particluarRequestingUser)
     res.status(200).json(users)
 }
-const getUser = (req, res) => {}
-const createUser = (req, res) => {}
+const getUser = (req, res) => { }
+
+const createUser = async (req, res) => {
+    const { first_name, middle_name, last_name, profile_img, gender, country, dob, email,password } = req.body
+
+    // console.log(req.body);
+
+    try {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        
+        const userExits = await User.findOne({ email })
+        const existingUser = await User.find({ email: 'fisayobadina@gmail.com' });
+
+        // If user exist return the user
+        if (userExits) return res.status(400).json({ message: 'User already exist' });
+
+        console.log(existingUser);
+        
+        // If user doesn't exist, Create user 
+       const newUser = new User({
+            first_name, middle_name, last_name, profile_img, gender, country, email, dob, password: hashedPassword, friends: [], blockers: [], requests: [],    
+       })
+       const savedUser = await newUser.save();
+
+        // Generate a JWT
+        const token = jwt.sign({ userId: savedUser._id }, SECRET_KEY, { expiresIn: '1h' });
+
+        // Return the token to the client
+        res.status(201).json({ token });
+    } catch (err) {
+        console.error("Error during registration:", err);
+        res.status(500).json({ error: 'Error registering user' });
+    }
+}
 const updateUser = (req, res) => {}
 const deleteUser = (req, res) => {}
 
