44bfe3f9bf084d73ea8784a304e239a9493cbe4c
fisayo-dev <olufisayobadina@gmail.com>
2024-09-23 14:19:44 +0100

    restyled the code

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,121 +6,13 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const SECRET_KEY = process.env.SECRET_KEY;
+async function callUsers() {
+    const allUsers = await User.findOne({ email })
+    return allUsers;
+} 
 
-const fakeUsers = [
-    {
-        id: 1,
-        name: 'Fisayo',
-        email: 'fisayo@fisayo.com',
-        friends: [
-            101
-        ],
-        requests: [
-            100
-        ]
-    },
-    {
-        id: 2,
-        name: 'Daniel',
-        email: 'daniel@daniel.com',
-        friends: [
-            101
-        ],
-        requests: [
-            100
-        ]
-    },
-    {
-        id: 3,
-        name: 'Sofia',
-        email: 'sofia@sofia.com',
-        friends: [
-            101
-        ],
-        requests: [
-            100
-        ]
-    },
-    {
-        id: 4,
-        name: 'Gbenga',
-        email: 'gbenga@gbenga.com',
-        friends: [
-            101
-        ],
-        requests: [
-            100
-        ]
-    },
-    {
-        id: 5,
-        name: 'Elizabeth',
-        email: 'lizzy@gmail.com',
-        friends: [
-            100,
-        ],
-          requests: [
-            101
-        ]
-    },
-    {
-        id: 6,
-        name: 'Demilade',
-        email: 'ladezy@gmail.com',
-        friends: [
-            100,
-        ],
-          requests: [
-            101
-        ]
-    },
-    {
-        id: 7,
-        name: 'Ayokunle',
-        email: 'kunzi@yahoo.com',
-        friends: [
-            100,
-        ],
-          requests: [
-            101
-        ]
-    },
-    {
-        id: 8,
-        name: 'Esther',
-        email: 'kings@gmail.com',
-        friends: [
-            100,
-        ],
-          requests: [
-            101
-        ]
-    },
-    {
-        id: 9,
-        name: 'Bisilola',
-        email: 'bode@gmail.com',
-        friends: [
-            100,
-        ],
-          requests: [
-            101
-        ]
-    },
-    {
-        id: 10,
-        name: 'Folusho',
-        email: 'shola@yahoo.com',
-        friends: [
-            100,
-        ],
-          requests: [
-            101
-        ]
-    },
-]
 const getUsers = (req, res) => { 
-    res.status(201).json(fakeUsers)
+    res.status(201).json(callUsers())
 }
 const getFriends = (req, res) => {
     const personId = parseInt(req.body.personId );
@@ -163,17 +55,14 @@ const createUser = async (req, res) => {
         const hashedPassword = await bcrypt.hash(password, 10);
         
         const userExits = await User.findOne({ email })
-        const existingUser = await User.find({ email: 'fisayobadina@gmail.com' });
 
         // If user exist return the user
         if (userExits) return res.status(400).json({ message: 'User already exist' });
-
-        console.log(existingUser);
         
         // If user doesn't exist, Create user 
-       const newUser = new User({
+        const newUser = new User({
             first_name, middle_name, last_name, profile_img, gender, country, email, dob, password: hashedPassword, friends: [], blockers: [], requests: [],    
-       })
+        })
        const savedUser = await newUser.save();
 
         // Generate a JWT
