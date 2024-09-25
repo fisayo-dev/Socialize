f765ce2dac62b28250920e83d8e56d9244194720
fisayo-dev <olufisayobadina@gmail.com>
2024-09-25 02:46:46 +0100

    added the show popup card logic

--- frontend/src/layouts/chatLayout.jsx
+++ frontend/src/layouts/chatLayout.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom";
-import { ChatProfile, ChatSidebar } from "../components";
+import { ChatProfile, ChatSidebar, DarkBg, SearchUsersCard } from "../components";
+import { useState } from "react";
 const ChatLayout = () => {
+  const [show,setShow ] = useState(false)
   return (
-    <div className="chats-app-flex">
-      <ChatSidebar />
+    <div className={`chats-app-flex`}>
+      {show && <SearchUsersCard show={show} setShow={setShow} />}
+      {show && <DarkBg />}
+      <ChatSidebar setShow={setShow} />
       <Outlet />
       <ChatProfile />
     </div>
