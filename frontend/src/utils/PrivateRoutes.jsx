8c8d6e6241519f08853321ade700022163896e25
fisayo-dev <olufisayobadina@gmail.com>
2024-09-26 21:35:48 +0100

    tweaked naviagte function

--- frontend/src/utils/PrivateRoutes.jsx
+++ frontend/src/utils/PrivateRoutes.jsx
@@ -1,9 +1,10 @@
-import { Navigate, Outlet } from "react-router-dom";
+import { useNavigate, Outlet } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 
 const PrivateRoutes = () => {
+  const navigate = useNavigate()
   const { user } = useAuth();
-  return user ? <Outlet /> : <Navigate to="/login" />;
+  return user ? <Outlet /> : navigate('/login');
 };
 
 export default PrivateRoutes;
