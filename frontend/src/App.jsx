7d2ef84d604f5dfdcfbfecd7f49624d954d4ce03
fisayo-dev <olufisayobadina@gmail.com>
2024-09-27 20:52:21 +0100

    removed fecthing sample api logic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,23 +9,13 @@ import { Landing, Login, Signup } from "./pages/App";
 import { ChatLayout } from "./layouts";
 import { PageNotFound } from "./pages";
 import { Chats, ChatsId } from "./pages/User";
-import PrivateRoutes  from "./utils/PrivateRoutes";
+import PrivateRoutes from "./utils/PrivateRoutes";
 
 function App() {
-  const [message, setMessage] = useState("");
-
-  const fetchMainText = async () => {
-    const res = await fetch("/api"); // Make the request to the backend
-    const data = await res.json();
-    setMessage(data.message);
-  };
-  useEffect(() => {
-    fetchMainText();
-  }, []);
   const router = createBrowserRouter(
     createRoutesFromElements(
       <>
-        <Route path="/" element={<Landing message={message} />} />
+        <Route path="/" element={<Landing />} />
         <Route path="*" element={<PageNotFound />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
