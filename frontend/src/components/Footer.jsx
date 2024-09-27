7e4be089738ded2d5ef356c3e6a2cede04407c81
fisayo-dev <olufisayobadina@gmail.com>
2024-09-27 16:29:27 +0100

    tweked the ui function of the footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,10 +1,10 @@
 const Footer = () => {
   return (
-    <div className="fixed bottom-0 w-full">
+    <div className=" bottom-0 w-full">
       <div className="app-container backdrop-blur-md ">
-        <div className="py-10 items-center justify-center flex">
+        <div className="py-10 items-center justify-center gap-10 flex">
           <h1 className="font-bold">All rights reserved</h1>
-          {/* <p>Copyright</p> */}
+          <p>Copyright 2024</p>
         </div>
       </div>
     </div>
