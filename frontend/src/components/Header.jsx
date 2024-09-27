03c54574d30511069e5e244c9aab362eb0605768
fisayo-dev <olufisayobadina@gmail.com>
2024-09-27 16:28:54 +0100

    gave the header z index

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <div className="fixed top-0 w-full">
+    <div className="fixed z-20 top-0 w-full">
       <div className="app-container backdrop-blur-md ">
         <div className="py-10 items-center justify-center text-center flex">
           <h1 className="text-1xl font-bold">Socialize</h1>
