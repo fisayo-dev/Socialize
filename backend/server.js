fc196fe691549c6c935fffe589136a96c21f5efd
fisayo-dev <olufisayobadina@gmail.com>
2024-10-05 13:09:06 +0100

    did some installations and cors setup

--- backend/server.js
+++ backend/server.js
@@ -15,15 +15,21 @@ const port = process.env.PORT || 7000;
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+// Enable CORS for all routes
+app.use(cors()); // Allows all origins by default
 // Routes
 app.use('/api/users', usersRouter)
 app.use('/api/chats', chatsRouter)
 
-// CORS
-app.use(cors({
-  origin: 'https://websocialize.vercel.app', // Replace with your Vercel domain
+
+// Or configure CORS to allow specific origins
+const corsOptions = {
+  origin: 'https://websocialize.vercel.app', // Replace with your frontend domain
   credentials: true
-}));
+};
+
+// Apply CORS globally or on specific routes
+app.use(cors(corsOptions));
 
 
 try {
